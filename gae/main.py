1374616c454cb5bbe113b3408daf562f3d87aec7
Fergus Gallagher <fergus@gooses.co.uk>
2016-02-28 19:42:03 +0000

    abstract

--- gae/main.py
+++ gae/main.py
@@ -1,83 +1,38 @@
 import logging, json
 
 from google.appengine.api import urlfetch
-from google.appengine.ext import ndb
-
 from flask import Flask, g, redirect, request, session, url_for, Response
 from twilio.rest import TwilioRestClient
 from flask.ext.restful import Resource, Api as RestApi
 
-# Needs ACCOUNT_SID AUTH_TOKEN TWILIO_FROM FLOODWATCH_URL
-import localsettings
+from localsettings import ACCOUNT_SID, AUTH_TOKEN, TWILIO_FROM, TEST_MOBILE, FLOODWATCH_URL
+from models import Setting, Person
 import footpath
-
-
-class Person(ndb.Model):
-    name = ndb.StringProperty(required=True)
-    setting_id = ndb.StringProperty(required=True)
-    trigger_level = ndb.StringProperty(required=True)
-    mobile = ndb.StringProperty(required=True)
-    last_level = ndb.StringProperty(required=True)
-
-
-class Setting(ndb.Model):
-    id = ndb.StringProperty(required=True)
-    name = ndb.StringProperty(required=True)
-    normal = ndb.IntegerProperty(required=True)
-    levels = ndb.StringProperty(required=True)
-
-client = TwilioRestClient(localsettings.ACCOUNT_SID, localsettings.AUTH_TOKEN)
-
-app = Flask(__name__)
-api = RestApi(app)
 
 API = "/api"
 ADMIN = "/admin"
 
+app = Flask(__name__)
+api = RestApi(app)
+
+client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
+
 def sendMessage(number, message):
     client.messages.create(
         to = number,
-        from_ = localsettings.TWILIO_FROM,
+        from_ = TWILIO_FROM,
         body = message
     )
 
-@app.route(API+"/dial")
-def dial():
-    client.calls.create(from_=localsettings.TWILIO_FROM,
-                        to=localsettings.TEST_MOBILE,
-                        url=url_for('.voice',  _external=True))
-    return "OK"
-
-@app.route(API+"/voice")
-def voice():
-    xml= """<?xml version="1.0" encoding="UTF-8" ?>
-<Response>
-     <Say>Hello World</Say>
-</Response>
-    """
-    return Response(xml, mimetype='text/xml')
-
-@api.resource(API + '/hello')
-class Hello(Resource):
-    def get(self):
-        url = localsettings.FLOODWATCH_URL
-        result = urlfetch.fetch(url)
-        if result.status_code != 200:
-            return {"error": result.status_code}, result.status_code
-
-        ret = json.loads(result.content)
-        return {"payload": ret["payload"]}
-
-
 @api.resource(ADMIN + '/create')
-class CreateDatastore(Resource):
+class CreateDataStore(Resource):
     def get(self):
         Person(
-            name="Test User",
-            setting_id="default",
-            trigger_level="low",
-            mobile=localsettings.TEST_MOBILE,
-            last_level="very_low"
+            name = "Test User",
+            setting_id = "default",
+            trigger_level = "low",
+            mobile = TEST_MOBILE,
+            last_level = "very_low"
         ).put()
 
         Setting(
@@ -94,7 +49,7 @@ class ListAll(Resource):
     def getLevel(self, s, levels, current_level):
         delta = s.normal - current_level
         level = None
-        for f in  reversed(levels):
+        for f in reversed(levels):
             if (delta > f["level"]):
                 level = f["name"]
                 break
@@ -104,16 +59,19 @@ class ListAll(Resource):
         return (level, delta)
 
     def get_current_level(self):
-        url = localsettings.FLOODWATCH_URL
+        url = FLOODWATCH_URL
         result = urlfetch.fetch(url)
         if result.status_code != 200:
-            raise {"error": result.status_code}
+            return (None, result.status_code)
 
         ret = json.loads(result.content)
-        return int(ret["payload"]["value"])
+        return (int(ret["payload"]["value"]), None)
 
     def get(self):
-        current_level = self.get_current_level()
+        (current_level, err_code) = self.get_current_level()
+
+        if err_code is not None:
+            raise {"error": err_code}
 
         ret = []
 
@@ -129,9 +87,13 @@ class ListAll(Resource):
             level_idx = level_names.index(level)
             last_level_idx = level_names.index(p.last_level)
 
-            alert = (p.last_level != level and
-                (level_idx >= trigger_idx) or
-                (level_idx < trigger_idx and last_level_idx >= trigger_idx))
+            alert = (
+                p.last_level != level and
+                (
+                    (level_idx >= trigger_idx) or
+                    (level_idx < trigger_idx and last_level_idx >= trigger_idx)
+                )
+            )
 
             msg = "%s: %s norm:%s delta:%s level:%s last:%s trigger:%s level:%s alert:%s" % (
                 p.name, p.mobile, s.normal, delta, level, p.last_level, trigger_idx, level_idx, alert)
@@ -141,28 +103,24 @@ class ListAll(Resource):
             if alert:
                 p.last_level = level
                 p.put()
-                m = "Dear {pname}, the level at {sname} is currently *{level}* ({delta}cm)".format(
+                m = "Dear {pname}, the level at {sname} is currently {level} ({delta}cm)".format(
                     pname=p.name,
                     sname=s.name,
                     level=levels[level_idx]["desc"],
                     delta=delta
                 )
                 logging.info("Sending %s to %s" % (m, p.mobile))
-                sendMessage(p.mobile, m)
+                try:
+                    sendMessage(p.mobile, m)
+                except Exception as e:
+                    logging.error(e)
 
         return ret
 
 @api.resource(ADMIN + '/ping')
 class Ping(Resource):
     def get(self):
-        sendMessage(localsettings.TEST_MOBILE, "Hello to you too" )
-        return {"hello": "to you too"}
-
-
-@api.resource(ADMIN + '/sendhello/<number>')
-class SendHello(Resource):
-    def get(self, number):
-        sendMessage(number, "Hello to you too" )
+        sendMessage(TEST_MOBILE, "Hello to you too" )
         return {"hello": "to you too"}
 
 if __name__ == "__main__":
---
+++ gae/models.py
@@ -0,0 +1,15 @@
+from google.appengine.ext import ndb
+
+class Person(ndb.Model):
+    name = ndb.StringProperty(required=True)
+    setting_id = ndb.StringProperty(required=True)
+    trigger_level = ndb.StringProperty(required=True)
+    mobile = ndb.StringProperty(required=True)
+    last_level = ndb.StringProperty(required=True)
+
+
+class Setting(ndb.Model):
+    id = ndb.StringProperty(required=True)
+    name = ndb.StringProperty(required=True)
+    normal = ndb.IntegerProperty(required=True)
+    levels = ndb.StringProperty(required=True)
