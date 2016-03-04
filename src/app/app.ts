b55c2f2371f4abd7a235275a0cb939d4e47ce804
Fergus Gallagher <fergus@gooses.co.uk>
2016-03-04 20:46:40 +0000

    Fix redirect

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,5 @@
 import {Component, ViewEncapsulation} from "angular2/core";
-import {
-RouteConfig,
-ROUTER_DIRECTIVES
-} from "angular2/router";
+import {RouteConfig, Redirect, ROUTER_DIRECTIVES} from "angular2/router";
 
 import {Default, Chacks} from "./components";
 
@@ -14,9 +11,8 @@ import {Default, Chacks} from "./components";
     directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-    { path: "/chacks", component: Chacks, name: "ChacksRedirect" },
+    new Redirect({ path: "/chacks", redirectTo: ['/Chacks'] }),
     { path: "/sites/pigeonslock", component: Default, name: "Default", useAsDefault: true },
     { path: "/sites/chacks", component: Chacks, name: "Chacks" }
 ])
 export class AppCmp { }
-
