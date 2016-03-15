3cdd464f9ea682696a0f100c4c31940cf0d978a2
Fergus Gallagher <fergus@gooses.co.uk>
2016-03-15 20:32:14 +0000

    expand about page

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,5 +1,6 @@
 import {Component} from "angular2/core";
 import {ROUTER_DIRECTIVES} from "angular2/router";
+import {defaultConfig} from "../../../config";
 
 @Component({
     selector: "about",
@@ -8,6 +9,7 @@ import {ROUTER_DIRECTIVES} from "angular2/router";
     directives: [...ROUTER_DIRECTIVES]
 })
 class About {
+    public feedback = defaultConfig.feedback;
 }
 
 export {About};
