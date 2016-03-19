22e3dc6f676289299dc8da9c90e5098823476961
Fergus Gallagher <fergus@gooses.co.uk>
2016-03-19 18:15:14 +0000

    abstract redirects

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,23 @@
+declare var _: any;
+
 import {Component, ViewEncapsulation} from "angular2/core";
 import {RouteConfig, Redirect, Route, ROUTER_DIRECTIVES} from "angular2/router";
 
 import {Footpath, Chacks, About, Jane, IndexComponent} from "./components";
+
+let redirectPaths: string[][] = [
+    ["/about", "/About"],
+    ["/help", "/About"],
+    ["/chacks", "/Chacks"],
+    ["/flightsmill", "/Chacks"],
+    ["/jane", "/Jane"],
+    ["/pigeonslock", "/Footpath"]
+];
+
+let redirects = _.map(redirectPaths, (r) => {
+    let [path, to] = r;
+    return new Redirect({ path, redirectTo: [to] });
+});
 
 @Component({
     selector: "app",
@@ -10,12 +26,14 @@ import {Footpath, Chacks, About, Jane, IndexComponent} from "./components";
     encapsulation: ViewEncapsulation.None,
     directives: [ROUTER_DIRECTIVES]
 })
-@RouteConfig([
-    new Route({ path: "/", component: IndexComponent, name: "Index" }),
-    new Route({ path: "/sites/pigeonslock", component: Footpath, name: "Footpath" }),
-    new Route({ path: "/sites/chacks", component: Chacks, name: "Chacks" }),
-    new Route({ path: "/sites/jane", component: Jane, name: "Jane" }),
-    new Route({ path: "/pages/about", component: About, name: "About" }),
-    new Redirect({ path: "/chacks", redirectTo: ['/Chacks'] })
-])
+@RouteConfig(
+    [
+        new Route({ path: "/", component: IndexComponent, name: "Index", useAsDefault: true }),
+        new Route({ path: "/sites/pigeonslock", component: Footpath, name: "Footpath" }),
+        new Route({ path: "/sites/chacks", component: Chacks, name: "Chacks" }),
+        new Route({ path: "/sites/jane", component: Jane, name: "Jane" }),
+        new Route({ path: "/pages/about", component: About, name: "About" }),
+        ...redirects
+    ]
+)
 export class AppCmp { }
