d616762a29ab5d706f85d55cd95d1a9bcc6a963c
Fergus Gallagher <fergus@gooses.co.uk>
2016-03-21 19:37:54 +0000

    tweak vars

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 declare var _: any;
 
-import {Component, ViewEncapsulation} from "angular2/core";
+import {Component} from "angular2/core";
 import {RouteConfig, Redirect, Route, ROUTER_DIRECTIVES} from "angular2/router";
 
 import {About, IndexComponent} from "./components";
 import * as Sites from "./sites";
-
 
 let redirectPaths: string[][] = [
     ["/about", "/About"],
@@ -13,36 +12,36 @@ let redirectPaths: string[][] = [
     ["/contact", "/About"]
 ];
 
-let redirects = _.map(redirectPaths, (r : string[]) => {
+let redirects = _.map(redirectPaths, (r: string[]) => {
     let [path, to] = r;
     return new Redirect({ path, redirectTo: [to] });
 });
 
-let routes =     [
-        new Route({ path: "/", component: IndexComponent, name: "Index", useAsDefault: true }),
-        new Route({ path: "/pages/about", component: About, name: "About" }),
-        ...redirects
-    ];
+let routes = [
+    new Route({ path: "/", component: IndexComponent, name: "Index", useAsDefault: true }),
+    new Route({ path: "/pages/about", component: About, name: "About" }),
+    ...redirects
+];
 
 for (let name of Object.keys(Sites)) {
     let path = `/sites/${name.toLowerCase()}`;
     let component = Sites[name];
-    routes.push(new Route({path, component, name}));
+    routes.push(new Route({ path, component, name }));
     for (let path of Sites[name].getRouteAliases()) {
-        routes.push(new Redirect({ path, redirectTo: [name] }));
+        let redirectTo = [name];
+        routes.push(new Redirect({ path, redirectTo }));
     }
 }
 
 // Fallback
 routes.push(
-    new Redirect({path: '/**', redirectTo: ['/Index']})
+    new Redirect({ path: '/**', redirectTo: ['/Index'] })
 );
 
 @Component({
     selector: "app",
     moduleId: module.id,
     template: "<router-outlet></router-outlet>",
-    encapsulation: ViewEncapsulation.None,
     directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig(routes)
