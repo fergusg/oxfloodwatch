a1fed00ae17481fa1e7e5da2f3e5f29e19399f7f
Fergus Gallagher <fergus@gooses.co.uk>
2016-03-29 20:50:31 +0100

    make id unique

--- src/app/components/about/gallery.ts
+++ src/app/components/about/gallery.ts
@@ -1,28 +1,40 @@
-import {Component} from "angular2/core";
+import {Component, ElementRef} from "angular2/core";
 import {ROUTER_DIRECTIVES} from "angular2/router";
 
 declare var $: any;
+declare var _: any;
 
 @Component({
     selector: "gallery",
     templateUrl: "./gallery.html",
     moduleId: module.id,
-    directives: [...ROUTER_DIRECTIVES]
+    directives: [...ROUTER_DIRECTIVES],
+    styles: [
+        `
+            .carousel {
+                margin-bottom: 10px;
+                margin-top: 10px;
+            }
+        `
+    ]
 })
 class Gallery {
-    public images = [
-        {
-          src: "dry.jpg",
-          descr: "Normal"
-        },
-        {
-          src: "wet.jpg",
-          descr: "Wet"
-        }
-    ];
+    public images: any[];
+    public id: string;
+
+    constructor(private elem: ElementRef) {
+        this.id = "gallery-" + Math.random().toString(36).slice(2);
+        this.images = _.map(
+            [
+                ["dry", "Normal"],
+                ["wet", "Wet"]
+            ],
+            (d => { return { src: `${d[0]}.jpg`, descr: d[1] }; })
+        );
+    }
 
     ngOnInit() {
-        $('.carousel').carousel({
+        $(this.elem.nativeElement).find(".carousel").carousel({
             interval: 4000
         });
     }
