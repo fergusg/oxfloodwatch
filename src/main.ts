430b66a2e2067c3612734beb01d46a847868a955
Fergus Gallagher <fergus@gooses.co.uk>
2016-05-01 18:58:03 +0100

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,21 @@ declare var jQuery: any;
 const applicationCache = window.applicationCache;
 
 import "rxjs/add/operator/map";
-import "rxjs/add/operator/debounceTime";
-import "rxjs/add/observable/of"; // gives us .of()
 import "rxjs/add/observable/timer";
 import "rxjs/add/observable/fromEvent";
-import "rxjs/add/observable/throw";
 import "rxjs/add/operator/timeout";
-import "rxjs/add/operator/delay";
-import "rxjs/add/operator/do";
 import "rxjs/add/operator/switchMap";
 import "rxjs/add/operator/catch";
 import "rxjs/add/operator/publish";
 
 import "./app/util/jquery.toaster";
 
-import {provide, enableProdMode} from "angular2/core";
+import {enableProdMode} from "angular2/core";
 import {bootstrap} from "angular2/platform/browser";
-import {APP_BASE_HREF} from "angular2/platform/common";
 import {ROUTER_PROVIDERS} from "angular2/router";
 import {HTTP_PROVIDERS, JSONP_PROVIDERS} from "angular2/http";
 
-// import {Angulartics2} from 'angulartics2/index';
+import {Angulartics2} from 'angulartics2/index';
 
 import {AppCmp} from "./app/app";
 
@@ -35,7 +29,6 @@ if (applicationCache) {
             title: 'Update available',
             message: 'Reload for latest version'
         });
-        // window.location.reload();
     }, false);
 }
 
@@ -43,6 +36,5 @@ bootstrap(AppCmp, [
     ...ROUTER_PROVIDERS,
     ...HTTP_PROVIDERS,
     ...JSONP_PROVIDERS,
-    // Angulartics2,
-    provide(APP_BASE_HREF, { useValue: "/" })
+    Angulartics2
 ]);
