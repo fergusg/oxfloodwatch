f50f032da1171d15cf7418718eb6de4e2224ca71
Fergus Gallagher <fergus@gooses.co.uk>
2017-02-05 17:11:29 +0000

    Resurrect guage chart

---
+++ src/app/components/home/gauge.simple/gauge.ts
@@ -0,0 +1,30 @@
+import {Component, ElementRef, OnChanges, SimpleChange} from "angular2/core";
+
+declare var $: any;
+declare var _: any;
+
+@Component({
+    selector: "gauge",
+    moduleId: module.id,
+    templateUrl: "./gauge.html",
+    styleUrls: ["./gauge.css"],
+    inputs: ["range", "delta", "plotBands"]
+})
+export default class GaugeComponent implements OnChanges {
+    private delta: number;
+    private deltaInches: string;
+
+    constructor(private elem: ElementRef) {
+    }
+
+    public ngOnChanges(changes: { [propName: string]: SimpleChange }) {
+        if (changes["delta"] && _.isFinite(changes["delta"].currentValue)) {
+            let d = changes["delta"].currentValue;
+            if (_.isFinite(d)) {
+                this.delta = d;
+                this.deltaInches = (this.delta / 2.54).toFixed(1);
+            }
+        }
+    }
+
+}
